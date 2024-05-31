a79346a2c13a19c131af031746e8404b9cf2b315
Aslanbek Nilov <145422367+niiilov@users.noreply.github.com>
2024-05-31 13:10:47 +0000

    S

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
     return (
-        <div className="header">
-            
+        <div className="bg-orange-500 w-full flex jusitfy-center">
+            <div className="w-full max-w-[1440px] flex justi">
+                <img src="/logo.svg" alt="logo" />
+                <div></div>
+            </div>
         </div>
     )
 }
