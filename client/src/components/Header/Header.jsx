c4953272ef75a4526ee89b0f0aa675eadf72734f
niiilov <nilovaslanbek2006@gmail.com>
2024-05-31 16:55:16 +0300

    Update Navbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
+import Logo from './assets/logo.svg'
+
 export default function Header() {
     return (
-        <div className="bg-orange-500 w-full flex jusitfy-center">
-            <div className="w-full max-w-[1440px] flex justi">
-                <img src="/logo.svg" alt="logo" />
-                <div></div>
+        <nav className="bg-orange-500 w-full py-4 flex justify-center">
+            <div className="w-full max-w-[1440px] flex items-center justify-between">
+                <header>
+                    <img className=' h-4' src={Logo} />
+                </header>
+                <div className="flex">
+
+                </div>
             </div>
-        </div>
+        </nav>
     )
 }
